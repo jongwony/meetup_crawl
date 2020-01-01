98b8a0d813f4376e61d1055a982464b42783eaba
jongwony <lastone9182@gmail.com>
2020-01-02 05:42:40 +0900

    cookie handle success

--- crawl.py
+++ crawl.py
@@ -5,7 +5,7 @@ from common.chrome import init, wait_for
 from common.io import config, script_path
 
 
-def login(driver):
+def login(driver, event_id):
     driver.get('https://secure.meetup.com/login/?_locale=ko-KR')
     print('login...')
     wait_for(driver, 'link_text', '구글 계정으로 로그인하기').click()
@@ -15,14 +15,9 @@ def login(driver):
     wait_for(driver, 'id', 'passwordNext').click()
     # wait for cookies load
     wait_for(driver, 'id', 'mainKeywords')
+    driver.get(f'https://www.meetup.com/ko-KR/awskrug/events/{event_id}/attendees/')
+    wait_for(driver, 'css_selector', 'ul.list--infinite-scroll.attendees-list.list')
     print('success login!')
-
-
-@contextmanager
-def login_check(driver):
-    if wait_for(driver, 'link_text', '로그인') is not None:
-        login(driver)
-    yield
 
 
 @contextmanager
@@ -32,9 +27,9 @@ def driver_init(headless, user_data):
     driver.quit()
 
 
-def refresh_token(headless, user_data):
+def refresh_token(event_id, headless, user_data):
     with driver_init(headless, user_data) as driver:
-        login(driver)
+        login(driver, event_id)
         with open(script_path(config('cookie')), 'wb') as f:
             cookies = driver.get_cookies()
             pickle.dump(cookies, f)
@@ -42,9 +37,9 @@ def refresh_token(headless, user_data):
 
 
 @contextmanager
-def get_token(headless=True, user_data=False):
+def get_token(event_id, headless=True, user_data=False):
     try:
         with open(script_path(config('cookie')), 'rb') as f:
             yield pickle.load(f)
     except FileNotFoundError:
-        yield refresh_token(headless, user_data)
+        yield refresh_token(event_id, headless, user_data)
--- manage_attendees.py
+++ manage_attendees.py
@@ -1,66 +1,97 @@
 from json import JSONDecodeError
-from urllib.parse import quote_plus
 from pprint import pprint
+from urllib.parse import quote_plus
 
 import jmespath
 import requests
 
 from crawl import get_token
 
+
+class MeetUpSession(requests.Session):
+    def __init__(self, event_id):
+        super().__init__()
+        self.event_id = event_id
+        self.member_id = None
+        self.amount = None
+        self.api_url = 'https://www.meetup.com/mu_api/urlname/events/eventId/attendees'
+        self.req_headers = {
+            'accept': '*/*',
+            'accept-encoding': 'gzip, deflate, br',
+            'content-type': 'application/json; charset=utf-8',
+            'cache-control': 'no-cache',
+            'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36',
+            'sec-fetch-mode': 'cors',
+            'sec-fetch-site': 'same-origin',
+            'x-meetup-activity': 'standardized_url=%2Furlname%2Fevents%2FeventId%2Fattendees&standardized_referer=%2Furlname%2Fevents%2FeventId',
+            'referer': f'https://www.meetup.com/ko-KR/awskrug/events/{self.event_id}/attendees/',
+        }
+        with get_token(self.event_id, headless=False) as cookies:
+            for cookie in cookies:
+                self.cookies.set(cookie['name'], cookie['value'], domain='.meetup.com')
+
+    def set_member_id(self, member_id):
+        self.member_id = member_id
+
+    def set_amount(self, amount):
+        self.amount = amount
+
+    def set_cookies(self, response):
+        set_cookie = response.headers['set-cookie']
+        dict_cookies = dict([
+            cookie for x in set_cookie.split()
+            if len(cookie := x.strip(';').split('=')) == 2
+        ])
+        self.req_headers['x-mwp-csrf'] = dict_cookies['x-mwp-csrf-header']
+        for k, v in dict_cookies.items():
+            self.cookies.set(k, v, domain='.meetup.com')
+
+    def get(self, **kwargs):
+        assert self.event_id is not None
+        get_attendee_params = {'queries': f"(endpoint:awskrug/events/{self.event_id}/rsvps,meta:(method:get),params:(desc:!t,fields:'answers,pay_status,self,web_actions,attendance_status',only:'answers,response,attendance_status,guests,member,pay_status,updated',order:time),ref:eventAttendees_awskrug_{self.event_id},type:attendees)"}
+        response = super().get(
+            self.api_url,
+            params=get_attendee_params,
+            headers=self.req_headers,
+        )
+        self.set_cookies(response)
+        return response
+
+    def post(self, **kwargs):
+        assert self.member_id is not None
+        assert self.amount is not None
+        assert self.event_id is not None
+        post_attendee_data = quote_plus(f"(endpoint:awskrug/events/{self.event_id}/payments,meta:(method:post),params:(amount:{self.amount},eventId:'{self.event_id}',member:{self.member_id},urlname:awskrug),ref:markAsPaid)")
+        self.req_headers['content-type'] = 'application/x-www-form-urlencoded'
+
+        response = super().post(
+            self.api_url,
+            data=b'queries=' + bytes(post_attendee_data, 'utf-8'),
+            headers=self.req_headers,
+        )
+        self.set_cookies(response)
+        return response
+
+
 if __name__ == '__main__':
     # call attendees api
-    headers = {
-        'accept': '*/*',
-        'accept-encoding': 'gzip, deflate, br',
-        'content-type': 'application/json; charset=utf-8',
-        'cache-control': 'no-cache',
-        'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36',
-    }
-    api_url = 'https://www.meetup.com/mu_api/urlname/events/eventId/attendees'
-    event_id = '267489117'
-    amount = 5000
-    member_id = 250533185
+    session = MeetUpSession('267489117')
 
-    get_attendee_params = {'queries': f"(endpoint:awskrug/events/{event_id}/rsvps,meta:(method:get),params:(desc:!t,fields:'answers,pay_status,self,web_actions,attendance_status',only:'answers,response,attendance_status,guests,member,pay_status,updated',order:time),ref:eventAttendees_awskrug_{event_id},type:attendees)"}
-    with get_token(headless=False) as cookies:
-        sess = requests.Session()
-        for cookie in cookies:
-            sess.cookies.set(cookie['name'], cookie['value'])
-        resp = sess.get(
-            api_url,
-            params=get_attendee_params,
-            headers=headers,
-        )
-        attendees = resp.json()
-
+    resp = session.get()
+    attendees = resp.json()
     get_attendee_query = 'responses[0].value[].{updated: updated, id: member.id, name: member.name, pay_status: pay_status, response: response, bio: answers[0].answer}'
     attendees_data = jmespath.search(get_attendee_query, attendees)
     pprint(attendees_data)
 
     # post
     '''
-{"responses":[{"ref":"markAsPaid","value":{"id":"43401196","confirm_code":"M43401196"},"meta":{"server":"ip-10-192-12-176","requestId":"210497bc-89a3-4712-9418-4e3de626d00c","endpoint":"awskrug/events/267489117/payments","statusCode":200}}]}
+    {"responses":[{"ref":"markAsPaid","value":{"id":"43401196","confirm_code":"M43401196"},"meta":{"server":"ip-10-192-12-176","requestId":"210497bc-89a3-4712-9418-4e3de626d00c","endpoint":"awskrug/events/267489117/payments","statusCode":200}}]}
     '''
-    headers['content-type'] = 'application/x-www-form-urlencoded'
-    headers['sec-fetch-mode'] = 'cors'
-    headers['sec-fetch-site'] = 'same-origin'
-    headers['x-meetup-activity'] = 'standardized_url=%2Furlname%2Fevents%2FeventId%2Fattendees&standardized_referer=%2Furlname%2Fevents%2FeventId'
-
-    post_attendee_data = quote_plus(f"(endpoint:awskrug/events/{event_id}/payments,meta:(method:post),params:(amount:{amount},eventId:'{event_id}',member:{member_id},urlname:awskrug),ref:markAsPaid)")
-    print(post_attendee_data)
-    with get_token(headless=False) as cookies:
-        sess = requests.Session()
-        for cookie in cookies:
-            sess.cookies.set(cookie['name'], cookie['value'])
-        print(b'queries=' + bytes(post_attendee_data, 'utf-8'))
-        resp = sess.post(
-            api_url,
-            data=b'queries=' + bytes(post_attendee_data, 'utf-8'),
-            headers=headers,
-        )
-        print(resp.request.headers)
-        try:
-            ack = resp.json()
-        except JSONDecodeError:
-            ack = resp.content
+    session.set_amount(5000)
+    session.set_member_id(251549990)
+    resp = session.post()
+    try:
+        ack = resp.json()
+    except JSONDecodeError:
+        ack = resp.content
     pprint(ack)
